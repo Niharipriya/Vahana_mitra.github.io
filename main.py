7cfb55ddbcff240919edb163bcd69ee6763b5100
RohanWasala0 <118281554+RohanWasala0@users.noreply.github.com>
2025-07-07 14:14:01 +0530

    just the base setup

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def base_page():
+    return render_template("home_page.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
