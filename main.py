7d52d9143a23b7cd63c9dce0fbcbbc8a3352b701
Nihari Priya Reddy <niharipriyareddy@Mac.home>
2025-07-17 09:47:29 -0400

    Updated UI and logic for signup, login, and landing page

--- main.py
+++ main.py
@@ -129,7 +129,8 @@ def profile():
                 fullname = signup_form.fullname.data,
                 password_hash = hashed_password,
                 email = signup_form.email.data,
-                phone = signup_form.phone.data,
+                phone = request.form.get('full_phone')
+,
             )
             db.session.add(user)
             db.session.commit()
@@ -208,6 +209,13 @@ def logout():
     flash('Logged out successfully.', 'info')
     return redirect(url_for('profile'))
 
+@app.route("/tabs")
+def view_tabs():
+    trucks = Truck.query.all()
+    loads = Load.query.all()
+    return render_template("tabs.html", trucks=trucks, loads=loads)
+
+
 @app.route("/admin")
 @login_required
 def admin():
@@ -217,8 +225,9 @@ def admin():
 
     user = User.query.all()
     return render_template('admin.html', users=user)
-
+ 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
