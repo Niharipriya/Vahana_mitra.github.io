15900d8e2b465ae342f12b76d8b988e4159e6a37
RohanWasala0 <118281554+RohanWasala0@users.noreply.github.com>
2025-07-24 07:15:45 +0530

    few

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'dev.db')
+        'sqlite:///' + os.path.join(basedir, 'instance/dev.db')
 
 class TestingConfig(Config):
     TESTING = True
