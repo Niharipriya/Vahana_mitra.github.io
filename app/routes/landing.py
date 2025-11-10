960493a4d1c4c1d4b417eef72fb640dcbfb5bee8
RohanWasala0 <rohan.wasala0@gmail.com>
2025-11-10 18:39:08 +0530

    Booking Card changes

--- app/routes/landing.py
+++ app/routes/landing.py
@@ -1,38 +1,41 @@
 from flask import (
     Blueprint,
     render_template,
-    flash, 
     session,
-    redirect, url_for, request, 
-    app
+    request,
 )
-from flask_login import current_user
 
 from app.models import Truck, Load
 from app.constants.session_keys import SessionKeys
-from app.constants.variable_constants import Truck_conts, Load_conts
 from app.form import TruckRegistrationForm, LoadRegistrationForm
 from app.utils import _redirect_save, make_admin_user, make_testing_data
 
 bp = Blueprint("landing", __name__, url_prefix="")
 
-@bp.route("/", methods=['POST', 'GET'])
+
+@bp.route("/", methods=["POST", "GET"])
 def index():
     truck_form = TruckRegistrationForm()
-    session[SessionKeys.TRUCK_CURRENT_LOCATION] = ''
+    session[SessionKeys.TRUCK_CURRENT_LOCATION] = ""
     load_form = LoadRegistrationForm()
-    session[SessionKeys.LOAD_PICKUP_LOCATION] = ''
+    session[SessionKeys.LOAD_PICKUP_LOCATION] = ""
 
     if truck_form.request_load.data:
         session[SessionKeys.IN_PROGRESS] = True
-        session[SessionKeys.BOOKING_TYPE] = Truck.__name__ #User is booking a load for there truck, i.e. entering there truck details 
-        session[SessionKeys.TRUCK_CURRENT_LOCATION] = truck_form.truck_current_location.data
+        session[SessionKeys.BOOKING_TYPE] = (
+            Truck.__name__
+        )  # User is booking a load for there truck, i.e. entering there truck details
+        session[SessionKeys.TRUCK_CURRENT_LOCATION] = (
+            truck_form.truck_current_location.data
+        )
 
         return _redirect_save(truck_form)
 
     if load_form.request_truck.data:
         session[SessionKeys.IN_PROGRESS] = True
-        session[SessionKeys.BOOKING_TYPE] = Load.__name__ #User is Booking a Truck for there load, ie entering there load details 
+        session[SessionKeys.BOOKING_TYPE] = (
+            Load.__name__
+        )  # User is Booking a Truck for there load, ie entering there load details
         session[SessionKeys.LOAD_PICKUP_LOCATION] = load_form.pickup_location.data
 
         return _redirect_save(load_form)
@@ -40,8 +43,9 @@ def index():
     if request.method == "POST":
         print(f"Form errors: {truck_form.errors} {load_form.errors}")
 
-    return render_template(
-        'landing.html',
-        truck_form=truck_form,
-        load_form=load_form
-    )
+    return render_template("landing.html", truck_form=truck_form, load_form=load_form)
+
+
+@bp.route("/testing_landingV2")
+def indexV2():
+    return render_template("landing_v3.html")
--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,4 @@
-from flask import (
-    session,
-    flash,
-    redirect, url_for
-)
+from flask import session, flash, redirect, url_for
 from flask_login import current_user
 from flask_wtf import FlaskForm
 from sqlalchemy.orm import DeclarativeMeta
@@ -12,31 +8,32 @@ from typing import Any, Type
 from app.constants.session_keys import SessionKeys
 from app.models import User
 
+
 def _redirect_save(form):
     session[SessionKeys.PENDING_FORM_DATA] = {
-        'fields': {
+        "fields": {
             field.name: field.data
             for field in form
-            if not('csrf_token' in field.name or 'submit' in field.name)
+            if not ("csrf_token" in field.name or "submit" in field.name)
         }
     }
     if not current_user.is_authenticated:
-        flash("Please Login or Sigup to continue your booking process", 'danger')
-        return redirect(url_for('auth.signup'))
-    return redirect(f'/booking/{session[SessionKeys.BOOKING_TYPE]}')
+        flash("Please Login or Sigup to continue your booking process", "danger")
+        return redirect(url_for("auth.signup"))
+    return redirect(f"/booking/{session[SessionKeys.BOOKING_TYPE]}")
+
 
 def autofill_fields(
-    prefix: str,
-    pending_form_data: dict,
-    form: FlaskForm
-) -> dict[str, Any]: 
+    prefix: str, pending_form_data: dict, form: FlaskForm
+) -> dict[str, Any]:
     autofill = {
         name.removeprefix(prefix): data
-        for name, data in pending_form_data.get('fields', {}).items()
+        for name, data in pending_form_data.get("fields", {}).items()
         if name.removeprefix(prefix) in form._fields and data is not None
     }
     print(f"Data from field autofill fields:{autofill}")
     return autofill
+
 
 def _autofill_db_dict(
     input_data: dict[str, Any],
@@ -52,9 +49,8 @@ def _autofill_db_dict(
     """
     # mapping from db column names → python attribute names
     column_map = {
-        col.name: attr 
-        for attr, col in database_class.__mapper__.columns.items()
-        }
+        col.name: attr for attr, col in database_class.__mapper__.columns.items()
+    }
 
     obj = database_class()
     for key, value in input_data.items():
@@ -73,16 +69,17 @@ def _autofill_db_dict(
 
     return obj
 
+
 def make_admin_user(admin_email, admin_password, db):
     """Create an admin user if not exists."""
     if admin_email and admin_password:
         admin_user = User.query.filter_by(email=admin_email).first()
         if not admin_user:
             admin_user = User(
-                fullname= "Admin",
-                email= admin_email,
-                password= admin_password,
-                is_admin=True
+                fullname="Admin",
+                email=admin_email,
+                password=admin_password,
+                is_admin=True,
             )
             db.session.add(admin_user)
             db.session.commit()
@@ -92,13 +89,14 @@ def make_admin_user(admin_email, admin_password, db):
     else:
         print("ADMIN_EMAIL environment variable not set. Cannot create admin user.")
 
+
 # function to populate testing data
 def make_testing_data(db):
     print("Populating testing data...")
     import json
     from .models import Truck, Load, User
 
-    with open("./testing_data.json", 'r') as file:
+    with open("./testing_data.json", "r") as file:
         testing_data = json.load(file)
 
     truck_testing_data = testing_data["trucks"]
@@ -117,46 +115,60 @@ def make_testing_data(db):
                 fullname=f"Test User {user_id}",
                 email=email,
                 password=f"password@{user_id}",
-                is_admin=False
+                is_admin=False,
             )
             db.session.add(user)
             db.session.flush()  # ensures ID assignment if autoincrement
             print(f"✅ Created user {user_id} ({email})")
             return user
 
-
     for truck_data in truck_testing_data:
-        user = make_get_user(truck_data['user_id'])
+        user = make_get_user(truck_data["user_id"])
 
         existing_truck = Truck.query.filter_by(
-            user_id = user.id,
-            vehicle_registration_number=truck_data['vehicle_registration_number']).first()
+            user_id=user.id,
+            vehicle_registration_number=truck_data["vehicle_registration_number"],
+        ).first()
         if existing_truck:
-            print(f"Truck with registration number {truck_data['vehicle_registration_number']} for user_id {user.id} already exists. Skipping.")
+            print(
+                f"Truck with registration number {truck_data['vehicle_registration_number']} for user_id {user.id} already exists. Skipping."
+            )
             continue
         else:
-            print(f"Truck created with registration number {truck_data['vehicle_registration_number']} for user_id {user.id}.")
+            print(
+                f"Truck created with registration number {truck_data['vehicle_registration_number']} for user_id {user.id}."
+            )
             truck = Truck(**truck_data)
             db.session.add(truck)
 
     for load_data in load_testing_data:
-        user = make_get_user(load_data['user_id'])
+        user = make_get_user(load_data["user_id"])
 
         from datetime import datetime
-        if 'pickup_datetime' in load_data and 'drop_datetime' in load_data:
-            load_data['pickup_datetime'] = datetime.fromisoformat(load_data['pickup_datetime'])
-            load_data['drop_datetime'] = datetime.fromisoformat(load_data['drop_datetime'])
+
+        if "pickup_datetime" in load_data and "drop_datetime" in load_data:
+            load_data["pickup_datetime"] = datetime.fromisoformat(
+                load_data["pickup_datetime"]
+            )
+            load_data["drop_datetime"] = datetime.fromisoformat(
+                load_data["drop_datetime"]
+            )
 
         existing_load = Load.query.filter_by(
-            user_id= user.id,
-            pickup_contact_name=load_data['pickup_contact_name']).first()
+            user_id=user.id, pickup_contact_name=load_data["pickup_contact_name"]
+        ).first()
         if existing_load:
-            print(f"Load with pickup contact name {load_data['pickup_contact_name']} for user_id {load_data['user_id']} already exists. Skipping.")
+            print(
+                f"Load with pickup contact name {load_data['pickup_contact_name']} for user_id {load_data['user_id']} already exists. Skipping."
+            )
             continue
         else:
-            print(f"Load created with pickup contact name {load_data['pickup_contact_name']} for user_id {load_data['user_id']}.")
+            print(
+                f"Load created with pickup contact name {load_data['pickup_contact_name']} for user_id {load_data['user_id']}."
+            )
             load = Load(**load_data)
             db.session.add(load)
 
     db.session.commit()
     print("Finished populating testing data.")
+
